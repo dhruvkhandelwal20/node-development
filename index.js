be900598240af20c74dc95fddc51ae0292abf980
Dhruv Khandelwal <dhruvkhandelwal39@gmail.com>
2022-02-16 11:12:23 +0530

    Practicing Node & Express

--- index.js
+++ index.js
@@ -1,50 +1,118 @@
-// Printing Hello World to kick-off things!
-console.log("Hello World!");
+// Importing our default express module from the downloaded express package.
+// Express is almost same as the HTTP Module. The only difference is that it is easier to write code in express.
+// Hence, express module is responsible for the deployment & management of huge applications.
 
-// Using File System Module of Node Js.
+// Creating a server using express
+const express = require("express");
 
-// Importing the FS Module
-const fs = require('fs');
-fs.writeFile('./newFile', 'This is a Sample Text Document.', 'utf-8', (err) => {
-    // The three parameters used in this method are: the fileName, content to be present in the file and the encoding mechanism.
-    // Additional parameter is the callback function of error.
-    if(err){
-        console.log(err);
-    }
-    console.log("File Created Successfully!");
+// Morgan is a package which determines each & every detail of the request such as method, time, etc.
+// Importing Morgan Package
+const morgan = require("morgan");
+
+// The express method is used to create a server.
+const app = express();
+
+// Middleware
+app.use(express.json());
+// express.json() method is used for parsing the body and is the method available in the express module.
+
+// morgan("dev") is a function. It is a Development dependency.
+app.use(morgan("dev"));
+
+app.get('/', (req, res) => {
+    res.send("GET METHOD IS WORKING FINE!");
 });
 
-// Creating Server using the Http Module
-
-// Importing the Http Module.
-const http = require('http');
-const server = http.createServer((req, res) => {
-    // Default Path of URL i.e. 127.0.0.1:8080 (localhost).
-         // console.log(req.url);
-        // res.end("Hello World!");
-
-// Configuring paths of the Request in the URL.
-// This is known as Routing.
-// The checking of paths of the URL in our server is known as Routing.
-    if(req.url === "/user"){
-        res.end("I am a User.");
-    }
-    if(req.url === "/login"){
-        res.end("Login Please!");
-    }
-    else{
-        res.end("Hello World!");
-    }
+app.post('/', (req, res) => {
+    res.send("POST METHOD IS WORKING FINE!");
 });
 
-server.listen(8080, () => {
+app.get('/user', (req, res) => {
+    res.send("It is a User Path working on GET Method.");
+})
+
+app.post('/user', (req, res) => {
+    res.send("It is a User Path working on POST Method.")
+})
+
+// Server Listening
+
+app.listen(8080, () => {
     console.log("Server is listening at Port: 8080");
 });
 
-// To Manage the N Number of Routes in a large server we use a Library called Express.js.
+// To parse the body data the client is sending via URL we'll use a middleware.
+// Middleware is a function which is going to be executed everytime whenever we receive some request.
+app.use((req, res, next) => {
+// The middleware app.use always contains a single callback function with three parameters named as req, res & next.
+console.log(req);
+// To prevent the server from getting stuck after completion of request next() method is used & it is the parameter which we have given in the middleware app.use().
+    next();
+});
 
-// Express is a Node.js web application framework. It is a Fast, Reliable, unopinionated, minimalist web framework for Node.js.
-// It helps us to implement our server in an easy way. It provides scalability.
+// Middleware to parse Current Date.
+app.use((req, res, next) => {
+  req.currentDate = new Date();
+  next();
+});
 
-// NPM: NODE PACKAGE MANAGER: IT CONTAINS THIRD PARTY PACKAGES DEVELOPED BY THE DEVELOPERS ACROSS THE GLOBE.
-// We can install express using npm. Command: npm install express
+app.get("/", (req, res) => {
+  console.log(req.currentDate);
+  res.send("Hello World!");
+});
+
+app.post("/user", (req, res) => {
+  const body = req.body;
+  console.log(req.body.number);
+  const square = body.number * body.number;
+  res.status(200).json({ square });
+  // res.send("POST METHOD IS WORKING!");
+});
+
+app.post("/user/:id", (req, res) => {
+    console.log(req.body);
+    console.log(req.query);
+    console.log(req.params);
+    res.status(200);
+  res.send("POST METHOD IS WORKING!");
+});
+
+app.put("/", (req, res) => {
+  res.send("PUT METHOD IS WORKING!");
+});
+
+app.patch("/", (req, res) => {
+  res.send("PATCH METHOD IS WORKING!");
+});
+
+app.delete("/", (req, res) => {
+  res.send("DELETE METHOD IS WORKING!");
+});
+
+app.head("/", (req, res) => {
+  res.send("HEAD METHOD IS WORKING FINE!");
+});
+
+app.listen(8080, () => {
+  console.log("App is listening at Port: 8080");
+});
+
+// These 5 Methods are used for splitting the url into multiple segments so as to perform different operations with the help of different methods.
+// These methods also defines the actions the client performs.
+
+// GET METHOD: To get some data from the server.
+// POST METHOD: To send some data to the server.
+// PUT METHOD:
+// DELETE METHOD: To delete some data from the database.
+// PATCH METHOD: To update some the data & the database.
+// HEAD METHOD:
+
+// THUNDER CLIENT IS A VSCODE EXTENSION Which mimics the client.
+
+// There are Three Types of Dependencies:
+// 1. Application Dependency: express
+// 2. Development Dependency: nodemon
+// 3. Global Dependency: It means that the package is available globally and can be used in every project which will be created in future.
+// We mostly install global package as a development package.
+
+// The Body of the request is the data the client is sending.
